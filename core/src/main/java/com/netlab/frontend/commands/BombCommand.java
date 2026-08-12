7f327f88be25460afd87a86b3031cd0988fb7883
Chordius <malau.jesaya@gmail.com>
2026-08-12 13:09:11 +0700

    Module 10: Update BombCommand and InputHandler to pass entities list to player.useBomb(bulletManager, entities)

--- core/src/main/java/com/netlab/frontend/commands/BombCommand.java
+++ core/src/main/java/com/netlab/frontend/commands/BombCommand.java
@@ -1,11 +1,27 @@
 package com.netlab.frontend.commands;
 
+import com.netlab.frontend.objects.GameObject;
 import com.netlab.frontend.objects.Player;
 import com.netlab.frontend.systems.BulletManager;
 
+import java.util.List;
+
 public class BombCommand implements Command {
+    private List<GameObject> entities;
+
+    public BombCommand(List<GameObject> entities) {
+        this.entities = entities;
+    }
+
+    public BombCommand() {
+        this(null);
+    }
+
     @Override
     public void execute(Player player, BulletManager bulletManager) {
-        player.useBomb(bulletManager);
+        player.useBomb(bulletManager, entities);
     }
+
+    public List<GameObject> getEntities() { return entities; }
+    public void setEntities(List<GameObject> entities) { this.entities = entities; }
 }
